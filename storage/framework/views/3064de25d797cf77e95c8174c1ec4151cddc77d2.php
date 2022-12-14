9813d2d90cf700415edae57a443948a57f43b853
MIKO SETIAWAN <98937432+mikosetiawan@users.noreply.github.com>
2022-12-14 09:36:24 +0700

    Add files via upload

---
+++ storage/framework/views/3064de25d797cf77e95c8174c1ec4151cddc77d2.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Qwordsku.com</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+
+<body>
+
+</body>
+
+<!-- COM NAVBAR -->
+<div class="container-fluid shadow-lg color-nav text-white py-1">
+    <p class="mx-2 my-2"> Cloud Hosting Indonesia dengan pelayanan terbaik dan harganya murah dengan diskon nama domain.</p>
+</div>
+
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg color-nav2 sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Qwordsku.com</a>
+        <button class="navbar-toggler border-0" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="bi bi-ui-radios-grid fs-3"></i>
+        </button>
+        <div class="collapse navbar-collapse px-3" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#migration">Migrasi Hosting</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#fiturUnggulan">Fitur Unggulan</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#payment" class="nav-link">Payment</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- HERO -->
+<div class="container" id="home">
+    <div class="row d-flex align-items-center">
+        <div class="col-lg-6">
+            <h1>Cloud Hosting Terbaik di Indonesia</h1>
+            <h3>#JagoHosting
+                <p class="py-5">Cloud Hosting Indonesia dengan pelayanan terbaik dan harga murah serta diskon nama domain.</p>
+                <a href="https://www.qwords.com/"><button class="btn-self">Cari Tahu Hosting?</button></a>
+        </div>
+        <div class="col-lg-6 px-5 py-5">
+            <img src="img/hero.png" class="w-100" alt="">
+        </div>
+    </div>
+</div>
+
+<!-- CONTENT Migrasi Hosting -->
+<div class="container-fluid color-nav3" id="migration">
+    <div class="row py-5 px-5">
+        <div class="py-5">
+            <h1 class="text-center">Migrasi Hosting ke Qwords dan Buat Website Dengan Mudah</h1>
+            <p class="text-center">Layanan unggulan dengan teknologi terbaik untuk website anda</p>
+        </div>
+        <div class="col-lg-6 border-0 text-center">
+            <img src="img/img_migrasi_gratis.svg" alt="">
+            <h3>Migrasi Gratis</h3>
+            <p>Pindahkan Hosting ke Qwords, proses aktivasi cepat dan tanpa ribet. Anda akan dibantu migrasi oleh staff kami tanpa tambahan biaya apapun.</p>
+            <button class="btn btn-outline-dark">Baca selengkapnya</button>
+        </div>
+        <div class="col-lg-6 border-0 text-center">
+            <img src="img/img_website_builder.svg" alt="">
+            <h3>Website Builder</h3>
+            <p>Nikmati Kemudahan membangun website secara instan dengan fasilitas 1 Click Installer dari softaculous serta Qwords Website Builder. Hanya dengan drag & drop didukung html5.</p>
+            <a href="https://www.qwords.com/migrasi-hosting/"><button class="btn btn-outline-dark">Baca selengkapnya</button></a>
+        </div>
+    </div>
+</div>
+
+<!-- FITUR UNGGULAN -->
+<div class="container-fluid" id="fiturUnggulan">
+    <div class="row text-center py-5 px-5">
+        <div class="py-5 text-center">
+            <h1>Fitur Unggulan WordPress Hosting</h1>
+        </div>
+        <div class="col-lg-4 d-flex py-3 justify-content-center">
+            <div class="card" style="width: 20rem;">
+                <div class="card-body">
+                    <div class="text-center">
+                        <img src="img/cloud-network.gif" class="w-75" alt="">
+                    </div>
+                    <div class="py-3">
+                        <h4 class="card-title py-3">Cloud Based Hosting</h4>
+                        <p class="card-text">WordPress Hosting Anda akan menggunakan Cloud Based Hosting sehingga dalam segi skalabilitas akan jauh lebih tinggi dibandingkan dengan Shared Hosting biasa.</p>
+                        <button class="btn-self2">Selengkapnya</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 d-flex py-3 justify-content-center">
+            <div class="card" style="width: 20rem;">
+                <div class="card-body">
+                    <div class="text-center">
+                        <img src="img/target.gif" class="w-75" alt="">
+                    </div>
+                    <div class="py-3">
+                        <h4 class="card-title py-3">Ultrafast SSD Storage</h4>
+                        <p class="card-text">Data Anda disimpan dalam SSD berkecepatan tinggi dan menggunakan system backup mingguan yang akan secara otomatis di backup dalam server kami.</p>
+                        <button class="btn-self2">Selengkapnya</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 d-flex py-3 justify-content-center">
+            <div class="card" style="width: 20rem;">
+                <div class="card-body">
+                    <div class="text-center">
+                        <img src="img/rocket.gif" class="w-75" alt="">
+                    </div>
+                    <div class="py-3">
+                        <h4 class="card-title py-3">Siap HTTP2 & SSL</h4>
+                        <p class="card-text">WordPress Hosting Anda telah mendukung penggunaan HTTP2 dan SSL yang dapat Anda dapatkan secara gratis ataupun berbayar.</p>
+                        <button class="btn-self2">Selengkapnya</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Pyment -->
+<div class="container-fluid" id="payment">
+    <div class="row py-5 px-5">
+        <h1 class="fs-2 text-center py-5">Pilih metode e-payment otomatis. Layanan Domain, Hosting, VPS langsung aktif tanpa perlu konfirmasi bayar.</h1>
+        <div class="col-lg-12 text-center">
+            <img src="img/all-payment.jpg" alt="">
+        </div>
+    </div>
+</div>
+
+<!-- FOOTER -->
+<div class="container-fluid color-nav">
+    <div class="row py-5 px-5 text-center text-white">
+        <div class="col-lg-12">
+        <p class="bi bi-c-circle-fill"> 2022 | mikosetiawan</p>
+        </div>
+    </div>
+</div>
+
+
+
+</html><?php /**PATH C:\xampp\htdocs\qwordsku.com\resources\views/welcome.blade.php ENDPATH**/ ?>
